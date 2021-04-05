2466edf61c3848f1a21d33756d1b8fd831fc2c16
Paul Fremantle <pzfreo@github.com>
2021-04-05 09:50:20 +0100

    be more verbose

--- purchase-login.py
+++ purchase-login.py
@@ -7,8 +7,8 @@ from pathlib import Path
 
 client_id = 'purchase'
 client_secret = os.environ['CLIENT_SECRET']
-oauth_host = os.environ['OAUTH2_HOST']
-oauth_port = os.environ['OAUTH2_PORT']
+oauth_host = os.getenv('OAUTH2_HOST', 'localhost')
+oauth_port = os.getenv('OAUTH2_PORT', '8080')
 
 base_url = 'http://'+oauth_host+':'+oauth_port
 device_url = '/realms/purchase/protocol/openid-connect/auth/device'
@@ -16,10 +16,16 @@ token_url = '/realms/purchase/protocol/openid-connect/token'
 
 
 #  call the device code api
+print("calling device api")
+
 r = requests.post(base_url+device_url, data = {'client_id':client_id}, auth=(client_id, client_secret))
 
 resp = r.json()
+print("received response")
+print (resp)
+
 # Open the returned URL
+print ("opening url", resp['verification_uri_complete'])
 webbrowser.open_new(resp['verification_uri_complete']);
 
 access_token = ""
@@ -33,23 +39,28 @@ reqdata = {
     'client_id': client_id
 }
 
+print ("polling for tokens while user does browser flow")
 # Poll for the tokens
 while not finished:
     r = requests.post(base_url+token_url, data = reqdata, auth=(client_id, client_secret))
+    print("recieved status code", r.status_code)
     if (r.status_code==200):
         tokendata = r.json()
         access_token = tokendata['access_token']
         refresh_token = tokendata['refresh_token']
         finished = True
         break
+    print ("waiting")
     time.sleep(resp['interval'])
+
 
 config = {
     'access_token': access_token,
     'refresh_token': refresh_token
 }
 
-
+print ("saving to ~/.config/purchase/secrets")
+print (yaml.dump(config))
 # save the tokens to the home directory
 
 home = str(Path.home())
